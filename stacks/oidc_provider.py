d4d8096b397b4a53fb064950ec7d4e1a441d5b18
Georges Roduit <georges.roduit@edeltech.ch>
2025-03-05 22:32:58 +0100

    Cleanup

--- stacks/oidc_provider.py
+++ stacks/oidc_provider.py
@@ -5,6 +5,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+
 class OidcProviderStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
